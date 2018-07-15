02e8e38be92dbff3d6129e9be6737e2758f224a7
Mathew Robinson <chasinglogic@gmail.com>
2018-07-15 18:41:11 -0400

    Expand tests to include queries from design
    Add missing operators to Lexer

--- tsk_lib/src/query/lexer.rs
+++ tsk_lib/src/query/lexer.rs
@@ -13,7 +13,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 use super::tokens::Token;
 use std::iter;
 use std::str;
@@ -41,10 +40,11 @@ impl<'a> Lexer<'a> {
         // next
         let mut s = self.current_char.unwrap().to_string();
         // Check for early end on one character tokens
-        if let Some(next_char) = self.content.peek() {
-            if end(*next_char) {
+        match self.content.peek() {
+            Some(c) => if end(*c) {
                 return s;
-            }
+            },
+            None => return s,
         }
 
         while let Some(c) = self.content.next() {
@@ -71,11 +71,13 @@ impl<'a> Lexer<'a> {
 
     fn number(&mut self) -> Token {
         let s = self.read_until(|c| {
-            (c.is_alphabetic() && c != '.' && c != '-') || c.is_whitespace() || c == ')'
+            (c.is_alphabetic() && c != '.' && c != '-' && c != ':') || c.is_whitespace() || c == ')'
         });
 
+        // It's a date if it has - in it
         if s.contains("-") {
             Token::from(s.as_ref())
+        // Otherwise we need to remove whitespace for the float to properly parsed
         } else {
             Token::from(
                 s.chars()
@@ -91,6 +93,14 @@ impl<'a> Lexer<'a> {
         Token::from(s.as_ref())
     }
 
+    fn keyword_string(&mut self) -> Token {
+        // Skip -
+        self.next_char();
+
+        let s = self.read_until(|c| c.is_whitespace() || c == ')' || c == '(');
+        Token::Str(s)
+    }
+
     fn next_char(&mut self) -> Option<char> {
         self.current_char = self.content.next();
         self.current_char
@@ -98,16 +108,26 @@ impl<'a> Lexer<'a> {
 
     fn token_from_char(&mut self, c: char) -> Token {
         match c {
-            '>' | '<' => if let Some('=') = self.content.peek() {
-                let mut s = c.to_string();
-                // Advance past the = sign.
-                // Safe to unwrap since the peek already showed us it was Some
-                s.push(self.next_char().unwrap());
-                Token::from(s.as_ref())
-            } else {
-                Token::from(c)
+            '>' | '<' | '^' | '!' => match self.content.peek() {
+                Some('=') => {
+                    let mut s = c.to_string();
+                    // Advance past the = sign.
+                    // Safe to unwrap since the peek already showed us it was Some
+                    s.push(self.next_char().unwrap());
+                    Token::from(s.as_ref())
+                }
+                Some('^') if c == '^' => {
+                    // Skip second ^
+                    self.next_char();
+                    Token::NLIKE
+                }
+                _ => Token::from(c),
             },
             '"' => self.string(),
+            '-' => match self.content.peek() {
+                Some('a'..='z') | Some('A'..='Z') => self.keyword_string(),
+                _ => self.number(),
+            },
             'a'..='z' | 'A'..='Z' => self.unquoted_string(),
             _num if c.is_digit(10) => self.number(),
             _ => Token::from(c),
@@ -135,81 +155,160 @@ impl<'a> Iterator for Lexer<'a> {
 pub mod tests {
     use super::*;
 
-    #[test]
-    fn test_all_unquoted_strings() {
-        let input = "this is a simple query";
-        let tokens: Vec<Token> = Lexer::from(input).collect();
-        assert_eq!(tokens[0], Token::Str("this".to_string()));
-        assert_eq!(tokens[1], Token::Str("is".to_string()));
-        assert_eq!(tokens[2], Token::Str("a".to_string()));
-        assert_eq!(tokens[3], Token::Str("simple".to_string()));
-        assert_eq!(tokens[4], Token::Str("query".to_string()));
-        assert_eq!(tokens.len(), 5);
+    struct LexerTest<'a> {
+        input: &'a str,
+        name: &'a str,
+        expected: Vec<Token>,
     }
 
     #[test]
-    fn test_unquoted_string_op_string() {
-        let input = "unquoted_string = \"this is a string\"";
-        let tokens: Vec<Token> = Lexer::from(input).collect();
-        assert_eq!(tokens[0], Token::Str("unquoted_string".to_string()));
-        assert_eq!(tokens[1], Token::EQ);
-        assert_eq!(tokens[2], Token::Str("this is a string".to_string()));
-        assert_eq!(tokens.len(), 3);
-    }
+    fn test_lexer() {
+        let tests = vec![
+            LexerTest {
+                input: "this is a simple query",
+                name: "all unquoted strings",
+                expected: vec![
+                    Token::Str("this".to_string()),
+                    Token::Str("is".to_string()),
+                    Token::Str("a".to_string()),
+                    Token::Str("simple".to_string()),
+                    Token::Str("query".to_string()),
+                ],
+            },
+            LexerTest {
+                input: "unquoted_string = \"this is a string\"",
+                name: "unquoted string op quoted string",
+                expected: vec![
+                    Token::Str("unquoted_string".to_string()),
+                    Token::EQ,
+                    Token::Str("this is a string".to_string()),
+                ],
+            },
+            LexerTest {
+                input: "5",
+                name: "one number",
+                expected: vec![Token::Float(5.0)],
+            },
+            LexerTest {
+                input: "(priority > 5)",
+                name: "grouped priority greater than 5",
+                expected: vec![
+                    Token::LP,
+                    Token::from("priority"),
+                    Token::from('>'),
+                    Token::Float(5.0),
+                    Token::RP,
+                ],
+            },
+            LexerTest {
+                input: "(priority > 5.0)",
+                name: "grouped priority greater than 5.0",
+                expected: vec![
+                    Token::LP,
+                    Token::from("priority"),
+                    Token::from('>'),
+                    Token::Float(5.0),
+                    Token::RP,
+                ],
+            },
+            LexerTest {
+                input:  "(unquoted_string >= 5.0 and other = \"other string\") or (mighty morphin power rangers)",
+                name: "complex expression",
+                expected: vec![
+                    Token::LP,
+                    Token::Str("unquoted_string".to_string()),
+                    Token::GTE,
+                    Token::Float(5.0),
+                    Token::AND,
+                    Token::Str("other".to_string()),
+                    Token::EQ,
+                    Token::Str("other string".to_string()),
+                    Token::RP,
+                    Token::OR,
+                    Token::LP,
+                    Token::Str("mighty".to_string()),
+                    Token::Str("morphin".to_string()),
+                    Token::Str("power".to_string()),
+                    Token::Str("rangers".to_string()),
+                    Token::RP,
+                ],
+            },
+            LexerTest {
+                input: "milk -and cookies",
+                name: "escaped keyword",
+                expected: vec![
+                    Token::Str("milk".to_string()),
+                    Token::Str("and".to_string()),
+                    Token::Str("cookies".to_string()),
+                ],
+            },
+            LexerTest {
+                input: "title ^ \"take out the trash\"",
+                name: "title like take out the trash",
+                expected: vec![
+                    Token::Str("title".to_string()),
+                    Token::LIKE,
+                    Token::Str("take out the trash".to_string()),
+                ]
+            },
+            LexerTest {
+                input: "(\"milk and sugar\") and priority > 5",
+                name: "grouped string query and priority greater than 5",
+                expected: vec![
+                    Token::LP,
+                    Token::Str("milk and sugar".to_string()),
+                    Token::RP,
+                    Token::AND,
+                    Token::Str("priority".to_string()),
+                    Token::GT,
+                    Token::Float(5.0),
+                ]
+            },
+            LexerTest {
+                input: "(priority > 5 and title ^ \"take out the trash\") or (context = \"work\" and (priority >= 2 or (\"my little pony\")))",
+                name: "very complex expression",
+                expected: vec![
+                    Token::LP,
+                    Token::Str("priority".to_string()),
+                    Token::GT,
+                    Token::Float(5.0),
+                    Token::AND,
+                    Token::Str("title".to_string()),
+                    Token::LIKE,
+                    Token::Str("take out the trash".to_string()),
+                    Token::RP,
+                    Token::OR,
+                    Token::LP,
+                    Token::Str("context".to_string()),
+                    Token::EQ,
+                    Token::Str("work".to_string()),
+                    Token::AND,
+                    Token::LP,
+                    Token::Str("priority".to_string()),
+                    Token::GTE,
+                    Token::Float(2.0),
+                    Token::OR,
+                    Token::LP,
+                    Token::Str("my little pony".to_string()),
+                    Token::RP,
+                    Token::RP,
+                    Token::RP
+                ]
+            }
+        ];
 
-    #[test]
-    fn test_unquoted_string_complex_op_string() {
-        let input = "unquoted_string >= \"this is a string\"";
-        let tokens: Vec<Token> = Lexer::from(input).collect();
-        assert_eq!(tokens[0], Token::Str("unquoted_string".to_string()));
-        assert_eq!(tokens[1], Token::GTE);
-        assert_eq!(tokens[2], Token::Str("this is a string".to_string()));
-        assert_eq!(tokens.len(), 3);
-    }
+        for test in tests.iter() {
+            println!("Running Test: {}", test.name);
 
-    #[test]
-    fn test_num_lexing() {
-        let mut input = "5";
-        let mut tokens: Vec<Token> = Lexer::from(input).collect();
-        assert_eq!(tokens[0], Token::Float(5.0));
+            let tokens: Vec<Token> = Lexer::from(test.input).collect();
 
-        input = "(priority > 5)";
-        tokens = Lexer::from(input).collect();
-        assert_eq!(tokens[0], Token::LP);
-        assert_eq!(tokens[1], Token::from("priority"));
-        assert_eq!(tokens[2], Token::from('>'));
-        assert_eq!(tokens[3], Token::Float(5.0));
-        assert_eq!(tokens[4], Token::RP);
+            for ind in 0..test.expected.len() {
+                let lexed_token = &tokens[ind];
+                let expected_token = &test.expected[ind];
+                assert_eq!(lexed_token, expected_token);
+            }
 
-        input = "(priority > 5.0)";
-        tokens = Lexer::from(input).collect();
-        assert_eq!(tokens[0], Token::LP);
-        assert_eq!(tokens[1], Token::from("priority"));
-        assert_eq!(tokens[2], Token::from('>'));
-        assert_eq!(tokens[3], Token::Float(5.0));
-        assert_eq!(tokens[4], Token::RP);
-    }
-
-    #[test]
-    fn test_complex_exp() {
-        let input = "(unquoted_string >= 5.0 and other = \"other string\") or (mighty morphin power rangers)";
-        let tokens: Vec<Token> = Lexer::from(input).collect();
-        assert_eq!(tokens[0], Token::LP);
-        assert_eq!(tokens[1], Token::Str("unquoted_string".to_string()));
-        assert_eq!(tokens[2], Token::GTE);
-        assert_eq!(tokens[3], Token::Float(5.0));
-        assert_eq!(tokens[4], Token::AND);
-        assert_eq!(tokens[5], Token::Str("other".to_string()));
-        assert_eq!(tokens[6], Token::EQ);
-        assert_eq!(tokens[7], Token::Str("other string".to_string()));
-        assert_eq!(tokens[8], Token::RP);
-        assert_eq!(tokens[9], Token::OR);
-        assert_eq!(tokens[10], Token::LP);
-        assert_eq!(tokens[11], Token::Str("mighty".to_string()));
-        assert_eq!(tokens[12], Token::Str("morphin".to_string()));
-        assert_eq!(tokens[13], Token::Str("power".to_string()));
-        assert_eq!(tokens[14], Token::Str("rangers".to_string()));
-        assert_eq!(tokens[15], Token::RP);
-        assert_eq!(tokens.len(), 16);
+            assert_eq!(tokens.len(), test.expected.len(),);
+        }
     }
 }
