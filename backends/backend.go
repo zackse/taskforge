515f0c8c38011223941f9fad4014ecf964e935d0
Mathew Robinson <chasinglogic@gmail.com>
2018-07-22 22:51:32 -0400

    Add doc comments

--- backends/backend.go
+++ backends/backend.go
@@ -6,6 +6,8 @@ import (
 	"github.com/chasinglogic/tsk/task"
 )
 
+// Backend is a task.List that supports saving and loading from
+// a data source.
 type Backend interface {
 	task.List
 
@@ -14,6 +16,15 @@ type Backend interface {
 	Load() error
 }
 
+// Config is a convenience type used to represent the unstructured config that
+// will be stored by the application. All backends should accept being decoded
+// using mapstructure from this type. Additionally, the Init() function should
+// return an error if a required argument was not provided.
+type Config map[string]interface{}
+
+// GetByName returns the appropriate Backend implementation by
+// a human redable string name. It returns a user friendly error
+// string if not found.
 func GetByName(name string) (Backend, error) {
 	switch name {
 	case "file":
