cf76a52c4d0bb3129f8d793be1a4a0680e5dc4f3
Mathew Robinson <chasinglogic@gmail.com>
2018-07-22 22:55:26 -0400

    Add doc comments and struct tags

--- task/task.go
+++ task/task.go
@@ -7,9 +7,11 @@ import (
 	"github.com/mongodb/mongo-go-driver/bson/objectid"
 )
 
+// Note is a note added to a task, analogous to a comment but only for ones
+// personal use.
 type Note struct {
-	CreatedDate time.Time
-	Body        string
+	CreatedDate time.Time `json:"created_date"`
+	Body        string    `json:"body"`
 }
 
 func (n Note) String() string {
@@ -21,6 +23,15 @@ func (n Note) String() string {
 	return string(jsn)
 }
 
+// NewNote will properly fill out the metadata of a note with the given body
+func NewNote(body string) Note {
+	return Note{
+		CreatedDate: time.Now(),
+		Body:        body,
+	}
+}
+
+// Task is a unit of work
 type Task struct {
 	ID            string    `json:"id"`
 	Title         string    `json:"title"`
@@ -41,13 +52,21 @@ func (t Task) String() string {
 	return string(jsn)
 }
 
+// New creates a task with the given title. This properly populates meta data
+// fields with non-zero values.
 func New(title string) Task {
 	t := Task{
 		Title:       title,
 		CreatedDate: time.Now(),
+		Context:     "default",
 	}
 
 	t.ID = objectid.New().Hex()
 
 	return t
+}
+
+// Complete completes this task.
+func (t *Task) Complete() {
+	t.CompletedDate = time.Now()
 }
