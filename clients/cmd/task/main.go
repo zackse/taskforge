3f54efbb66b400affcc6f942e4c8f425ef848dad
Mathew Robinson <chasinglogic@gmail.com>
2018-08-09 12:41:42 -0400

    move commands into task cli client package

--- clients/cmd/task/main.go
+++ clients/cmd/task/main.go
@@ -13,13 +13,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 package main
 
 import (
 	"fmt"
 
-	"github.com/chasinglogic/taskforge/commands"
+	"github.com/chasinglogic/taskforge/clients/cmd/task/commands"
 	"github.com/spf13/cobra"
 )
 
