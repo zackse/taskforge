7ea3133e4f154a667af78abb45c07a1705a254ee
Mathew Robinson <chasinglogic@gmail.com>
2018-08-13 13:36:26 -0400

    add license info to version output

--- cmd/task/main.go
+++ cmd/task/main.go
@@ -37,8 +37,22 @@ var versionCmd = &cobra.Command{
 	Aliases: []string{"v"},
 	Short:   "print version information",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("taskforge-%s-%s\n", commit, version)
-		fmt.Println(`license`)
+		fmt.Printf("taskforge-%s-%s\n%s\n", commit, version,
+			`
+Copyright 2018 Mathew Robinson <chasinglogic@gmail.com>. All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.`)
 	},
 }
 
