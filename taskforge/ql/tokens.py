801bbf7c14d10464367993691bef57d3afa74291
Mathew Robinson <chasinglogic@gmail.com>
2018-09-01 19:54:44 -0400

    Write query language tokens package and tests

---
+++ taskforge/ql/tokens.py
@@ -0,0 +1,79 @@
+import re
+from enum import Enum, auto
+
+DATE_REGEX = re.compile('^[0-9]{4}-[0-9]{2}-[0-9]{2}( [0-9]{2}:[0-9]{2})? ?(AM|PM|pm|am)?')
+NUMBER_REGEX = re.compile('^[0-9]{1,}')
+
+class Type(Enum):
+    GT = 'GT'
+    LT = 'LT'
+    GTE = 'GTE'
+    LTE = 'LTE'
+    EQ = 'EQ'
+    NE = 'NE'
+    LIKE = 'LIKE'
+    NLIKE = 'NLIKE'
+
+    AND = 'AND'
+    OR = 'OR'
+
+    LPAREN = 'LPAREN'
+    RPAREN = 'RPAREN'
+
+    EOF = 'EOF'
+    STRING = 'STRING'
+    NUMBER = 'NUMBER'
+    DATE = 'DATE'
+    BOOLEAN = 'BOOLEAN'
+
+    UNEXPECTED = 'UNEXPECTED'
+
+
+LITERAL_TYPES = {
+    "or": Type.OR,
+    "OR": Type.OR,
+    "and": Type.AND,
+    "AND": Type.AND,
+    "false": Type.BOOLEAN,
+    "False": Type.BOOLEAN,
+    "true": Type.BOOLEAN,
+    "True": Type.BOOLEAN,
+    ">": Type.GT,
+    "<": Type.LT,
+    ">=": Type.GTE,
+    "<=": Type.LTE,
+    "=": Type.EQ,
+    "!=": Type.NE,
+    "^=": Type.NE,
+    "^": Type.LIKE,
+    "~": Type.LIKE,
+    "^^": Type.NLIKE,
+    "!~": Type.NLIKE,
+    "(": Type.LPAREN,
+    ")": Type.RPAREN,
+}
+
+class Token:
+    """A query language lexical Token"""
+
+    def __init__(self, literal, token_type=None):
+        self.literal = literal
+        if token_type is not None:
+            self.token_type = token_type
+            return
+
+        if LITERAL_TYPES.get(literal):
+            self.token_type = LITERAL_TYPES[literal]
+        elif DATE_REGEX.match(literal):
+            self.token_type = Type.DATE
+        elif NUMBER_REGEX.match(literal):
+            self.token_type = Type.NUMBER
+        else:
+            self.token_type = Type.STRING
+
+    def __repr__(self):
+        return 'Token({}, {})'.format(self.token_type, self.literal)
+
+    def __eq__(self, other):
+        return (self.literal == other.literal and
+                self.token_type == other.token_type)
---
+++ tests/query/test_token.py
@@ -0,0 +1,36 @@
+import unittest
+from taskforge.ql.tokens import Token, Type
+
+
+class TestTokens(unittest.TestCase):
+
+    def test_token_types(self):
+        self.assertEqual(Token('or').token_type, Type.OR)
+        self.assertEqual(Token('OR').token_type, Type.OR)
+        self.assertEqual(Token('and').token_type, Type.AND)
+        self.assertEqual(Token('AND').token_type, Type.AND)
+        self.assertEqual(Token('false').token_type, Type.BOOLEAN)
+        self.assertEqual(Token('False').token_type, Type.BOOLEAN)
+        self.assertEqual(Token('true').token_type, Type.BOOLEAN)
+        self.assertEqual(Token('True').token_type, Type.BOOLEAN)
+        self.assertEqual(Token('>').token_type, Type.GT)
+        self.assertEqual(Token('<').token_type, Type.LT)
+        self.assertEqual(Token('>=').token_type, Type.GTE)
+        self.assertEqual(Token('<=').token_type, Type.LTE)
+        self.assertEqual(Token('=').token_type, Type.EQ)
+        self.assertEqual(Token('^=').token_type, Type.NE)
+        self.assertEqual(Token('!=').token_type, Type.NE)
+        self.assertEqual(Token('^').token_type, Type.LIKE)
+        self.assertEqual(Token('~').token_type, Type.LIKE)
+        self.assertEqual(Token('^^').token_type, Type.NLIKE)
+        self.assertEqual(Token('!~').token_type, Type.NLIKE)
+        self.assertEqual(Token('(').token_type, Type.LPAREN)
+        self.assertEqual(Token(')').token_type, Type.RPAREN)
+        self.assertEqual(Token('100').token_type, Type.NUMBER)
+        self.assertEqual(Token('hello world').token_type, Type.STRING)
+        self.assertEqual(Token('2018-01-01 10:00 PM').token_type, Type.DATE)
+        self.assertEqual(Token('2018-01-01 10:00 pm').token_type, Type.DATE)
+        self.assertEqual(Token('2018-01-01 10:00 AM').token_type, Type.DATE)
+        self.assertEqual(Token('2018-01-01 10:00 am').token_type, Type.DATE)
+        self.assertEqual(Token('2018-01-01 10:00').token_type, Type.DATE)
+        self.assertEqual(Token('2018-01-01').token_type, Type.DATE)
