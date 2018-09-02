a7da8dd8208482475bf3c2a21e0ad1f094142bfe
Mathew Robinson <chasinglogic@gmail.com>
2018-09-01 20:42:20 -0400

    Add cli entry point

---
+++ taskforge/cli/__init__.py
@@ -0,0 +1,31 @@
+import argparse
+
+from .add_cmd import add_cmd
+from .complete_cmd import complete_cmd
+# from .edit_cmd import edit_cmd
+from .next_cmd import next_cmd
+from .query_cmd import query_cmd
+from .todo_cmd import todo_cmd
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='task',
+    )
+    subparsers = parser.add_subparsers()
+    next_cmd(subparsers)
+    add_cmd(subparsers)
+    query_cmd(subparsers)
+    todo_cmd(subparsers)
+    complete_cmd(subparsers)
+    # edit_cmd(subparsers)
+
+    args = parser.parse_args()
+    if hasattr(args, 'func'):
+        args.func(args)
+    else:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
