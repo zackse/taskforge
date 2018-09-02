799cd7ecc57d4d76550399843842c89a54f24f2a
Mathew Robinson <chasinglogic@gmail.com>
2018-09-01 20:44:49 -0400

    Write next command

---
+++ taskforge/cli/next_cmd.py
@@ -0,0 +1,30 @@
+"""
+Implements the next subcommand
+"""
+
+from .utils import inject_list
+
+
+@inject_list
+def print_next(args, task_list=None):
+    """Print the current task in task_list"""
+    task = task_list.current()
+
+    if args.title_only:
+        print(task.title)
+    elif args.id_only:
+        print(task.id)
+    else:
+        print(task)
+
+
+def next_cmd(parser):
+    """Add the next command to parser"""
+    sub_parser = parser.add_parser(
+        'next',
+        aliases=['n'],
+        help='Print the next task in the list'
+    )
+    sub_parser.add_argument('--title-only', '-t', action='store_true')
+    sub_parser.add_argument('--id-only', '-i', action='store_true')
+    sub_parser.set_defaults(func=print_next)
