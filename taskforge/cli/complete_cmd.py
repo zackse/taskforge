d37b7e8da243d42347d41adbffe3eac8f85b709f
Mathew Robinson <chasinglogic@gmail.com>
2018-09-01 20:44:49 -0400

    Write complete command

---
+++ taskforge/cli/complete_cmd.py
@@ -0,0 +1,39 @@
+"""
+Implements the complete subcommand
+"""
+
+import sys
+
+from ..lists import NotFoundError
+from ..task import Task
+from .utils import inject_list
+
+
+@inject_list
+def complete_task(args, task_list=None):
+    """Print the current task in task_list"""
+    tasks = []
+    if args.id:
+        tasks = args.id
+    else:
+        try:
+            current = task_list.current()
+            tasks = [current.id]
+        except NotFoundError:
+            print('No ID given and no uncompleted task found')
+            sys.exit(0)
+
+    for task in tasks:
+        task_list.complete(task)
+
+
+def complete_cmd(parser):
+    """Add the next command to parser"""
+    sub_parser = parser.add_parser(
+        'complete',
+        aliases=['done', 'd'],
+        help='Complete tasks in the list. If no ID given will'
+        ' complete the current task.',
+    )
+    sub_parser.add_argument('id', metavar='ID', nargs='+', type=str)
+    sub_parser.set_defaults(func=complete_task)
