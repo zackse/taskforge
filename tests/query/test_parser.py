14e70a07eb6b92264a78e279f643e75421862583
Mathew Robinson <chasinglogic@gmail.com>
2018-09-19 14:35:24 -0400

    fmt: format using yapf

--- tests/query/test_parser.py
+++ tests/query/test_parser.py
@@ -6,74 +6,80 @@ from taskforge.ql import Parser
 from taskforge.ql.ast import AST, Expression
 from taskforge.ql.tokens import Token
 
-PARSER_TESTS = [
-    {
-        'name':
-        'simple parse',
-        'input':
-        'milk and cookies',
-        'output':
-        AST(
-            Expression(
+PARSER_TESTS = [{
+    'name':
+    'simple parse',
+    'input':
+    'milk and cookies',
+    'output':
+    AST(
+        Expression(
+            Token('and'),
+            left=Expression(Token('milk')),
+            right=Expression(Token('cookies'))), ),
+}, {
+    'name':
+    'boolean parse',
+    'input':
+    'completed = false',
+    'output':
+    AST(
+        Expression(
+            Token('='),
+            left=Expression(Token('completed')),
+            right=Expression(Token('false'))), ),
+}, {
+    'name': 'simple all string parse',
+    'input': 'milk -and cookies',
+    'output': AST(Expression(Token('milk and cookies'))),
+}, {
+    'name':
+    'complex parse',
+    'input':
+    '(priority > 5 and title ^ \'take out the trash\') or '
+    '(context = "work" and (priority >= 2 or ("my little pony")))',
+    'output':
+    AST(
+        Expression(
+            Token('or'),
+            right=Expression(
                 Token('and'),
-                left=Expression(Token('milk')),
-                right=Expression(Token('cookies'))), ),
-    },
-    {
-        'name':
-        'boolean parse',
-        'input':
-        'completed = false',
-        'output':
-        AST(
-            Expression(
-                Token('='),
-                left=Expression(Token('completed')),
-                right=Expression(Token('false'))), ),
-    },
-    {
-        'name': 'simple all string parse',
-        'input': 'milk -and cookies',
-        'output': AST(Expression(Token('milk and cookies'))),
-    },
-    {
-        'name':
-        'complex parse',
-        'input':
-        '(priority > 5 and title ^ \'take out the trash\') or '
-        '(context = "work" and (priority >= 2 or ("my little pony")))',
-        'output':
-        AST(
-            Expression(
-                Token('or'),
-                right=Expression(
-                    Token('and'),
-                    left=Expression(
-                        Token('='),
-                        left=Expression(Token('context')),
-                        right=Expression(Token('work'))),
-                    right=Expression(
-                        Token('or'),
-                        left=Expression(
-                            Token('>='),
-                            left=Expression(Token('priority')),
-                            right=Expression(Token('2'))),
-                        right=Expression(Token('my little pony'))),
-                ),
                 left=Expression(
-                    Token('and'),
-                    right=Expression(
-                        Token('~'),
-                        right=Expression(Token('take out the trash')),
-                        left=Expression(Token('title'))),
+                    Token('='),
+                    left=Expression(Token('context')),
+                    right=Expression(Token('work'))),
+                right=Expression(
+                    Token('or'),
                     left=Expression(
-                        Token('>'),
+                        Token('>='),
                         left=Expression(Token('priority')),
-                        right=Expression(Token('5'))),
-                ),
-            ), ),
-    },
-]
+                        right=Expression(Token('2'))),
+                    right=Expression(Token('my little pony'))),
+            ),
+            left=Expression(
+                Token('and'),
+                right=Expression(
+                    Token('~'),
+                    right=Expression(Token('take out the trash')),
+                    left=Expression(Token('title'))),
+                left=Expression(
+                    Token('>'),
+                    left=Expression(Token('priority')),
+                    right=Expression(Token('5'))),
+            ),
+        ), ),
+}, {
+    'name':
+    'parse bool infix',
+    'input':
+    'completed = false',
+    'output':
+    AST(
+        Expression(
+            Token('='),
+            left=Expression(Token('completed')),
+            right=Expression(Token('false'))))
+}]
 
 
 class ParserTests(unittest.TestCase):
