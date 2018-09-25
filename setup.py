950c1a8fc292515ff144a011d07bd01647524515
Mathew Robinson <chasinglogic@gmail.com>
2018-09-25 12:40:52 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 
 setup(
     name='taskforge-cli',
-    version='0.2.2',
+    version='0.2.3',
     url='https://github.com/chasinglogic/taskforge',
     license='AGPL-3.0',
     author='Mathew Robinson',
